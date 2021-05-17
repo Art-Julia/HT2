06b44e29108122ed6011de00e956556e320a9343
Yulia Arutyunyan <artn.julia@gmail.com>
2021-05-17 08:30:30 +0300

    reformating done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int TicketCost = 300_000; //Стоимость билета
-        int CostPerMile = 20; // стоимость одной бонусной мили
-        int TotalMiles = TicketCost/CostPerMile; // Мили за купленный билет
+        int ticketCost = 300_000; //Стоимость билета
+        int costPerMile = 20; // стоимость одной бонусной мили
+        int totalMiles = ticketCost / costPerMile; // Мили за купленный билет
         System.out.println("Total Miles:");
-        System.out.println(TotalMiles);
+        System.out.println(totalMiles);
 
     }
 }
